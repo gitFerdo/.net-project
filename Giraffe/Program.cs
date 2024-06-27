cbf8d2a9062387abf3fe083eff3ee93990e33632
amasha <fernandothushini8@gmail.com>
2024-06-27 16:01:22 +0530

    set up Program.cs

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
