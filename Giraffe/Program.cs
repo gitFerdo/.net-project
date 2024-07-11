fadc91e9ff5673878498245569ae23a5ecc478ce
amasha <fernandothushini8@gmail.com>
2024-07-11 23:28:55 +0530

    getting user inputs

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -10,43 +10,15 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(20);
-            Console.WriteLine(20 + 10);
-            Console.WriteLine(20 - 10);
-            Console.WriteLine(20 / 10);
-            Console.WriteLine(20 * 10);
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
 
-            // modulus operator
-            Console.WriteLine(5 % 2);
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
 
-            Console.WriteLine(5 - 2 + 10 * 2);
-            Console.WriteLine(5 - (2 + 10) * 2);
+            Console.WriteLine("Hello, " + name + "!" + "\nAge: " + age);
 
-            Console.WriteLine(5.0 + 3.1);
-
-            Console.WriteLine(5 / 2);
-            Console.WriteLine(5 / 2.0);
-
-            int num1 = 6;
-            Console.WriteLine(num1);
-
-            num1++;
-            Console.WriteLine(num1);
-
-            num1--;
-            Console.WriteLine(num1);
-
-            // Math methods
-            Console.WriteLine(Math.Abs(-14));
-            Console.WriteLine(Math.Pow(5, 2));
-            Console.WriteLine(Math.Pow(5.2, 2));
-            Console.WriteLine(Math.Sqrt(25));
-            Console.WriteLine(Math.Max(25, 15));
-            Console.WriteLine(Math.Min(25, 15));
-            Console.WriteLine(Math.Round(2.4));
-            Console.WriteLine(Math.Round(2.7));
-
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
