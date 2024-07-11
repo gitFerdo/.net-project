d4977d1469f58bb6700a74e43750d8e8257bf46b
amasha <fernandothushini8@gmail.com>
2024-07-11 15:44:25 +0530

    Variables - 1

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -10,14 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("     /|");
-           Console.WriteLine("    / |");
-           Console.WriteLine("   /  |");
-           Console.WriteLine("  /   |");
-           Console.WriteLine(" /    |");
-           Console.WriteLine("/_____|");
+            Console.WriteLine("There once was a man named John");
+            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But didn't like being 35");
 
-           Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
