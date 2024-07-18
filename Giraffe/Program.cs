ebfb52faaf4e5b24d1fd9fcdffeade074ab83714
amasha <fernandothushini8@gmail.com>
2024-07-18 23:26:23 +0530

    fix decimal error
    Building a Calculator

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -11,10 +11,10 @@ namespace Giraffe
         static void Main(string[] args)
         {
             Console.Write("Enter a number : ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Enter another number : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(num1 + num2);
 
--- Giraffe/obj/Debug/net8.0/Giraffe.AssemblyInfo.cs
+++ Giraffe/obj/Debug/net8.0/Giraffe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Giraffe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadc91e9ff5673878498245569ae23a5ecc478ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac5bb0acc722f43e53ec897a2bddc79d0aa1899")]
 [assembly: System.Reflection.AssemblyProductAttribute("Giraffe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Giraffe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
