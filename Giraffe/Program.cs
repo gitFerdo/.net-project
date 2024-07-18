31b78044f7a53fcb41f6cd88a19c80a46a8c9c11
amasha <fernandothushini8@gmail.com>
2024-07-18 23:31:12 +0530

    01
    Building a Mad Libs Game

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -10,13 +10,9 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number : ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter another number : ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine(num1 + num2);
+            Console.WriteLine("Roses are red");
+            Console.WriteLine("Violets are blue");
+            Console.WriteLine("I Love You");
 
             // Console.ReadLine();
         }
